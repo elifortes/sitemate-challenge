cdbb567c4d3a3e00f65a0a1ffd8f523fb2b9dd3c
Eliane Fortes <elimrfortes@gmail.com>
2024-06-10 18:37:55 +1000

    Add CORS to allow interaction with frontend

---
+++ server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 4000;
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let staticData = [{id: 1, description: " A Simple Challenge for Sitemate ", title: "Sitemate :)"}];
+
+app.post('/create', (req, res) => {
+  console.log('Create:', req.body);
+
+  let newRecord = {
+    id: parseInt(req.body.id),
+    description: req.body.description,
+    title: req.body.title,
+  }
+
+  staticData.push(newRecord);
+  res.status(201).send({ message: 'Created', data: req.body });
+});
+
+app.get('/read/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const item = staticData.find(data => data.id == id);
+
+  if (item) {
+    res.send(item);
+  } else {
+    res.status(404).send({ message: 'Not found' });
+  }
+});
+
+app.put('/update/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = staticData.findIndex(data => data.id == id);
+
+  if (index !== -1) {
+    staticData[index] = { ...staticData[index], ...req.body };
+    console.log('Update:', staticData[index]);
+    res.send({ message: 'Updated', data: staticData[index] });
+  } else {
+    res.status(404).send({ message: 'Not found' });
+  }
+});
+
+app.delete('/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = staticData.findIndex(data => data.id == id);
+
+  if (index !== -1) {
+    const deletedData = staticData.splice(index, 1);
+
+    console.log('Delete:', deletedData);
+    res.send({ message: 'Deleted', data: deletedData });
+  } else {
+    res.status(404).send({ message: 'Not found' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
